e8aaa9b092b2e60b84c56d67ffa287009da5cd09
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-13 13:56:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
-# Student name
-# Current date
+# Ethan Lawrence
+# September 13
 # String & Numeric Input 
 
 # Task 1
-# Comment
-# Comment
 
+# Input
+first_name = input('What is you first name:      ')
+program = input('What program are you attending at CTC:     ')
 
-
+# Output
+print(first_name + ' Is attending CTC in the ' + program +' Program this year.')
 
 # Task 2
-# Comment
-# Comment
 
+# input
+age = input('What is your current age:      ')
+age = int(age)
 
-
+# Output 
+print('You are ' + str(age) + ' years old and will be ' + str(age + 10) + ' in 10 years')
 
 # Task 3
-# Comment
-# Comment
 
+# input
+family_members = int(input('How many family members do you have:        '))
 
-
+# Output
+print('You have ' + str(family_members) + ' family members')
+print(f' You still have {family_members} family members')
 
 # Task 4
-# Comment
-# Comment
 
+# Input
+height_inches = float(input('How tall are you in inches (Ex.. 60):      '))
+CONVERSION_FACTOR = 2.54
 
-
-
+# Output
+height_centimeters = height_inches * CONVERSION_FACTOR
+print(f'You are {height_centimeters} cm. tall or {height_inches} in. tall')
+print('You are ' + str(height_centimeters) + 'cm. tall or ' + str(height_inches) + ' in. tall')
 
 # Task 5
 # Comment
